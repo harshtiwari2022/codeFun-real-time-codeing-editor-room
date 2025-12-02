a5c0498ae8d84e971b1d826d07c96aaa3c3fd46e
Harsh tiwari <nikkipandit8756@gmail.com>
2025-12-02 18:56:14 +0530

    Update Home.jsx

--- client/src/component/Home.jsx
+++ client/src/component/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom"
 import { v4 as uuidv4 } from "uuid";
 import { motion } from "framer-motion";
 import { SunIcon, MoonIcon } from "@heroicons/react/24/solid";
